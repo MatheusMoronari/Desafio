6620d1c2189b56f1f2c789dd968917708422bff6
MatheusMoronari <matheusmoronari@gamail.com>
2023-12-16 14:01:52 -0300

    Primeiro commit

---
+++ banco/connection.go
@@ -0,0 +1,24 @@
+package banco
+
+import (
+	"config"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenConnection() (*sql.DB, error) {
+	conf := config.GetDB()
+	sc := fmt.Sprintf(
+		"host=%s port=%s user= %s password = %s dbname = %s sslmode = disble",
+		conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
+	conn, err := sql.Open("postgres", sc)
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
---
+++ models/Insert.go
@@ -0,0 +1,15 @@
+package models
+
+func insert(todo Todo) (id int64, err error) {
+
+	conn, err := banco.ConnectToDB()
+	if err != nil {
+		return
+	}
+
+	defer conn.Close()
+
+	sql := `insert into todos (title, description, done ) VALUES ($1,$2,$3) RETURNING id`
+	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.done).Scan(&id)
+	return
+}
